0af74871984015292b5f3d753d7e3ab195dba22c
Arsalan Mateen <arsalanmateen.pk@gmail.com>
2026-09-23 14:16:03 +0500

    Time: 0 ms (100.00%) | Memory: 54.2 MB (15.33%) - LeetSync

---
+++ 27-remove-element/remove-element.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let temp = 0;
+    for (let i=0; i<nums.length; i++){
+       if(nums[i] != val)
+       {
+            if (i > temp) {
+                nums[temp] = nums[i];
+            }
+            temp++;
+       }
+    }
+    return temp;  
+};
